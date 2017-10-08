33c2642cda1e2a81a23afdda36a3fbd53c01cbc7
SeRGiO11 <sergio.anger@gmail.com>
2017-10-08 23:32:46 +0200

    Removed licesing from test files

--- Till/src/test/java/sj11/priceBasket/till/TillTest.java
+++ Till/src/test/java/sj11/priceBasket/till/TillTest.java
@@ -1,20 +1,3 @@
-/*
- * THIS SOFTWARE IS PROVIDED BY COSTAIN LTD ``AS IS'', WITH NO WARRANTY, TERM
- * OR CONDITION OF ANY KIND, EXPRESS OR IMPLIED, AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- * EVENT SHALL COSTAIN LTD BE LIABLE FOR ANY LOSSES, CLAIMS OR DAMAGES OF
- * WHATEVER NATURE, INCLUDING ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGES OR LOSSES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE.
- *
- * Copyright 2017 © Costain Ltd.
- * All Rights Reserved.
- */
 package sj11.priceBasket.till;
 
 import java.io.PrintStream;
@@ -72,7 +55,7 @@ public class TillTest extends AbstractTestUtils {
 
         till.charge(shoppingItems);
 
-        assertEquals(createExpectation("£2.10", "£1.70", "Bread","50%", "40p"), outContent.toString());
+        assertEquals(createExpectation("£2.10", "£1.70", "Bread", "50%", "40p"), outContent.toString());
     }
 
     @Test
@@ -81,7 +64,7 @@ public class TillTest extends AbstractTestUtils {
 
         till.charge(shoppingItems);
 
-        assertEquals(createExpectation("£3.55", "£3.15", "Bread","50%", "40p"), outContent.toString());
+        assertEquals(createExpectation("£3.55", "£3.15", "Bread", "50%", "40p"), outContent.toString());
     }
 
     @Test
@@ -90,7 +73,7 @@ public class TillTest extends AbstractTestUtils {
 
         till.charge(shoppingItems);
 
-        assertEquals(createExpectation("£2.10", "£1.70", "Bread","50%", "40p"), outContent.toString());
+        assertEquals(createExpectation("£2.10", "£1.70", "Bread", "50%", "40p"), outContent.toString());
     }
 
     @Test
@@ -110,7 +93,5 @@ public class TillTest extends AbstractTestUtils {
 
         assertEquals("One of the scanned products is not from this shop\n", outContent.toString());
     }
-
-
 
 }
